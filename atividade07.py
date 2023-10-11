4f32caa381069d1e847b9ef7770cd96f625dcfca
Luizyy <analuizynascimentodasilva@gmail.com>
2023-10-11 12:50:07 -0300

    ativ07.dolar

---
+++ atividade07.py
@@ -0,0 +1,4 @@
+# Digite um valor e veja quantos dolares voce poderá comprar: R$
+reais = float(input("insira o valor para convertimento de reais para dolares"))
+dolares = float(input("insira a cotação atual do dolar"))
+print ("seu dinheiro convertido é" , reais/dolares)
